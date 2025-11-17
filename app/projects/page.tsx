5640c3344fff08c855c7f6bb78490fef028c4e0a
Itule Godstime <itulegodstime3546@gmail.com>
2025-11-18 00:33:27 +0100

    updated projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,19 @@
+const  Projects= () => {
+    return ( 
+        <>
+            <div className = "text-white mx-2">
+            <h1 className="text-2xl text-center my-7 lg:text-3xl text-white font-extrabold font-[Poppins]" >My Work</h1>
+            </div>
+            <div className = "  ">
+
+                <iframe id = "weather app" src="https://advance-weather-app-sigma.vercel.app/" className = "w-full h-[600px] border border-amber-400 rounded-2xl">
+
+                </iframe>
+            </div>
+    
+        </>
+        
+     );
+}
+ 
+export default Projects ;
