ac6d0faab58b5d5fb1536dcac1e796a8d2ac1b68
Itule Godstime <itulegodstime3546@gmail.com>
2025-11-18 01:41:48 +0100

    sharp updates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,43 @@
 const Contacts = () => {
     return ( 
         <div>
-            <p>love</p>
+            <div className="flex justify-center gap-4 mt-10">
+      <a
+        href="#"
+        className="relative flex items-center justify-center w-16 h-16 rounded-full transition-all hover:bg-[#242c34]"
+      >
+        <svg
+          className="absolute w-[60%] h-full text-white hover:bounce-scale"
+          viewBox="0 0 24 24"
+        >
+          <path d="M20 2H4C2.897 2 2 2.897 2 4V20C2 21.103 2.897 22 4 22H20C21.103 22 22 21.103 22 20V4C22 2.897 21.103 2 20 2Z" />
+        </svg>
+      </a>
+
+      <a
+        href="#"
+        className="relative flex items-center justify-center w-16 h-16 rounded-full transition-all hover:bg-[#242c34]"
+      >
+        <svg
+          className="absolute w-[60%] h-full text-white hover:bounce-scale"
+          viewBox="0 0 24 24"
+        >
+          <circle cx="12" cy="12" r="10" />
+        </svg>
+      </a>
+
+      <a
+        href="#"
+        className="relative flex items-center justify-center w-16 h-16 rounded-full transition-all hover:bg-[#242c34]"
+      >
+        <svg
+          className="absolute w-[60%] h-full text-white hover:bounce-scale"
+          viewBox="0 0 24 24"
+        >
+          <path d="M3 12L21 12" />
+        </svg>
+      </a>
+    </div>
         </div>
      );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,9 +4,9 @@ const  Projects= () => {
             <div className = "text-white mx-2">
             <h1 className="text-2xl text-center my-6 lg:text-3xl text-white font-extrabold font-[Poppins]" >My Work</h1>
             </div>
-            <div className = "  ">
+            <div className = " ">
 
-                <iframe id = "weather app" src="https://advance-weather-app-sigma.vercel.app/" className = "w-full h-[600px] border border-amber-400 rounded-2xl">
+                <iframe id = "weather app" src="https://advance-weather-app-sigma.vercel.app/" className = "w-full h-[600px] border border-amber-400 rounded-2xl animate-[slideIn_10s_ease-out_forwards] ">
 
                 </iframe>
             </div>
